58af674c2b03478bf22dc924e00f5301c16fee18
amy788 <amityadavmailbox@gmail.com>
2025-10-17 23:25:57 +0530

    initial message

---
+++ Assignment_1_Task_1.py
@@ -0,0 +1,10 @@
+
+#Task 1
+x = input("Enter first number: ")
+y = input("Enter second number: ")
+a = int(x)
+b = int(y)
+print("Addition: ", a + b)
+print("Subtraction: ", a - b)
+print("Multiplication: ", a * b)
+print("Division: ", a / b)
